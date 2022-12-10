9c93d45730bf0eda2512fb235ba17715d1decacf
Miguel-UNSCH <miguelramirezgonzales48@gmail.com>
2022-12-10 17:20:15 -0500

    casi terminado el diseño de la Franklin

--- sistemas/js/inicio.js
+++ sistemas/js/inicio.js
@@ -9,3 +9,18 @@ buttonLeft.addEventListener('click', () => {
 buttonRight.addEventListener('click', () => {
     barraLateral.classList.remove('hide');
 })
+
+const photo = document.getElementById('photo');
+const opciones = document.getElementById('opciones');
+
+var estado = false
+photo.addEventListener('click', () => {
+    if (!estado){
+        opciones.style.display = 'block';
+        estado = true;
+    }
+    else {
+        opciones.style.display = 'none';
+        estado = false;
+    }
+})
